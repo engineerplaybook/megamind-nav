7e13b871ad3c247b9a3910b4ff5a1acfada94084
Anmol Thukral <thukral.anmol21@gmail.com>
2026-06-11 17:19:04 +0530

    chore: routing and sub apps update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+  },
   plugins: [react()],
   publicDir: false,
   build: {
